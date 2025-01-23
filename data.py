8d523b97623d76fdc726b986c4f322434cf2ce5d
Emanuelle Natal de Moura <156372524+Ema028@users.noreply.github.com>
2025-01-23 16:40:26 -0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {"text": "Sherlock Holmes é um personagem criado por Arthur Conan Doyle.", "answer": "True"},
+    {"text": "O endereço de Sherlock Holmes em Londres é 221B Baker Street.", "answer": "True"},
+    {"text": "Dr. Watson é o vilão nas histórias de Sherlock Holmes.", "answer": "False"},
+    {"text": "Sherlock Holmes usa um chapéu chamado 'deerstalker' em todas as histórias.", "answer": "False"},
+    {"text": "Sherlock Holmes toca violino.", "answer": "True"},
+    {"text": "A primeira história de Sherlock Holmes foi 'Um Estudo em Vermelho'.", "answer": "True"},
+    {"text": "Moriarty é o arqui-inimigo de Sherlock Holmes.", "answer": "True"},
+    {"text": "Sherlock Holmes trabalha como policial em Londres.", "answer": "False"},
+    {"text": "Sherlock Holmes tem um irmão chamado Mycroft Holmes.", "answer": "True"},
+    {"text": "Sherlock Holmes era conhecido por dizer a frase 'Elementar, meu caro Watson' em todas as histórias.", "answer": "False"},
+    {"text": "A Sra. Hudson é a governanta de Sherlock Holmes.", "answer": "True"},
+    {"text": "Sherlock Holmes resolveu mistérios usando apenas suas habilidades de dedução lógica.", "answer": "True"}
+]
